04a173573d74254763439b04a98fa9f92175b45b
wsandholthansen <william@sandholthansen.dk>
2022-12-01 13:39:27 +0100

    Update read_data

--- data_covid19_denmark_141122/read_data.py
+++ data_covid19_denmark_141122/read_data.py
@@ -8,6 +8,10 @@ import matplotlib.pyplot as plt
 # TODO - How to find infected? (we only get the day they become infected - no sense of time)
 # TODO - How to think about deaths (probably cumulative, but how vs infected?)
 
+lockdown1 = pd.Timestamp(year=2020,month=3,day=11)
+lockdown2 = pd.Timestamp(year=2021,month=12,day=21)
+
+
 file = '03_bekraeftede_tilfaelde_doede_indlagte_pr_dag_pr_koen.csv'
 df = pd.read_csv(r'C:\Users\willi\Documents\GitHub\Deep_Learning_Project_PINN\data_covid19_denmark_141122\Regionalt_DB\\'+file
                  , sep  =";", encoding= 'iso-8859-1')
@@ -17,7 +21,18 @@ df = df.groupby(['Region', 'Prøvetagningsdato']).sum()
 
 df = df.loc['Hovedstaden']
 
+plt.figure()
 plt.plot(np.arange(len(df.index)), df['Bekræftede tilfælde i alt'], label='Cases')
+plt.grid()
 
-plt.plot(np.arange(len(df.index)), df['Døde'], label='Death')
+#plt.figure()
+#plt.plot(np.arange(len(df.index)), df['Døde'], label='Death')
 
+
+data = {}
+for i, (date, row) in enumerate(df.iterrows()):
+    data[date] = df.iloc[i-7:i]['Bekræftede tilfælde i alt'].sum()
+
+# plt.figure()
+plt.plot(np.arange(len(df.index)),pd.Series(data))
+plt.show()
